d7ecffc5bfae8ae206c1308e9852db213970cfa1
Jon Harlem <jtharlem@gmail.com>
2015-11-17 10:25:58 -0800

    added basic streaming functionality

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+
+twitter.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+ 
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
