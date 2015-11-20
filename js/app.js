1e37b3b0500dc219cb3344b1109be5274be29348
Jon Harlem <jtharlem@gmail.com>
2015-11-20 15:12:41 -0800

    added structure for GET tweets and clean tweets

--- js/app.js
+++ js/app.js
@@ -1,18 +1,48 @@
-var twit = require('twitter'),
-	twitter = new twit({
+var Twit = require('twit');
+var T = new Twit({
 	  consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
 	  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 	});
-console.log(twitter)
 
-twitter.stream('statuses/filter', {track: 'javascript'}, function(stream) {
-  stream.on('data', function(tweet) {
-    console.log(tweet.text);
+var params = {
+  count: 200,
+  exclude_replies: true,
+  user_id: 1605,
+  screen_name:'sama'
+}
+
+var allCleanedData = [];
+
+function getTweetsAfter(maxIdValue){
+  var params = {
+    count: 200,
+    max_id: maxIdValue,
+    exclude_replies: true,
+    user_id: 1605,
+    screen_name:'sama'
+  }
+
+  T.get('statuses/user_timeline', params, function(err, data, response) {
+    // console.log(data)
+    var cleanedData = data.map(function(tweetObj) {
+      return {
+        text: tweetObj.text,
+        id: tweetObj.id
+      }
+    });
+    //ADD TO ALLCLEANEDDATA
+
+    //GET THE RIGHT MAX ID VALUE
+
+    getTweetsAfter(/*correctMax*/)
+
+    console.log(cleanedData);
+
   });
- 
-  stream.on('error', function(error) {
-    throw error;
-  });
-});
+
+}
+
+
+
